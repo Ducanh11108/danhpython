812ef53d9541c3cff886fcaa934548463610c393
Ducanh11108 <140966454+Ducanh11108@users.noreply.github.com>
2025-01-15 20:25:37 +0700

    Add files via upload

---
+++ TinhChuViDT.py
@@ -0,0 +1,18 @@
+# Tinh chu vi va dien tich hinh chu nhat
+
+chieuDai = 6
+chieuRong = 4
+
+name = "Tomy"
+
+chuVi = (chieuDai + chieuRong) * 2
+
+dienTich = chieuDai * chieuRong
+
+# In ket qua
+print("Chu vi la: ", chuVi)
+print("Dien tich la: ", dienTich)
+
+print(type(chieuDai))
+print(type(chieuRong))
+print(type(name))
